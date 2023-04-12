8a667e404f8909e2002f0fc9f5d0c12f9a605172
Mrunali <mrunaliurade26@gmail.com>
2023-04-12 22:49:45 +0530

    data

---
+++ Assign12.java
@@ -0,0 +1,27 @@
+
+class  IntegerI{
+ int i;
+ void printNum(){
+	 System.out.println(i);
+ }
+}
+
+class subClassJ extends IntegerI{
+	int j;
+	void printNum(){
+		
+	super.printNum();
+		System.out.println(j);
+	}
+}
+public class Assign12 {
+	
+		public static void main(String[] args) {
+			
+			
+	 subClassJ s=new subClassJ();
+	  s.i=4;
+	  s.j=1;
+	  s.printNum();
+	}
+	}
